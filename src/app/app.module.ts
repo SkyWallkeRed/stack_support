8d5a1bc08430ddd1095fcf68ffe40e8c909d1524
דוד אוחנה <davidmail000@gmail.com>
2018-07-24 22:28:55 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SheetComponent } from './sheet/sheet.component';
+// NGPRIME
+import { AccordionModule } from 'primeng/accordion';     // accordion and accordion tab
+import { MenuItem } from 'primeng/api';                 // api
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SidebarModule} from 'primeng/sidebar';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SheetComponent,
+    NavComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
